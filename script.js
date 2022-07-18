ef0d0f2a21ae6ca0c1bc287a2eb4d426a36f78f9
bharat <bharatkumarchuahan179@gmail.com>
2022-07-18 19:10:47 +0530

    theme-change-done

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
 
+let screen = document.querySelector("#app");
+let swap = document.querySelector("#swap");
+
+let day = true;
+Mainclass = ["day", "night"];
+dayClass = ["button_day", "button_night"];
+
+
+
+swap.addEventListener("click", (e) => {
+
+    if(day){
+        swap.classList.remove(swap.classList[0]);
+        swap.classList.add("button_night");
+        screen.classList.remove(screen.classList[0]);
+        screen.classList.add("night");
+        day = !day;
+
+    
+    }
+    else{
+        swap.classList.remove(swap.classList[0]);
+        swap.classList.add("button_day");
+        screen.classList.remove(screen.classList[0]);
+        screen.classList.add("day");
+        day = !day;
+
+    }
+})
